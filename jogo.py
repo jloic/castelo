7354d4d3eeeb301faac31c7fb7c6ba34ea0372e6
Loïc Jeannin <loic@digitalsk.com.br>
2017-12-08 21:21:56 -0200

    new monsters !!!

--- jogo.py
+++ jogo.py
@@ -3,61 +3,23 @@ import sys
 import time
 import random
 
-MAX_ROW = 3
-MAX_LINE = 3
+MAX_ROW = 4
+MAX_LINE = 4
 
-
-def fight_trex(player):
-    trex = Enemy('T-Rex', 20, 1, 2)
-    fight = Fight(player, trex)
-    fight.fight()
-
-def fight_dragon(player):
-    dragon = Enemy('Dragão', 30, 2, 6)
-    fight = Fight(player, dragon)
-    fight.fight()
-
-
-def get_sword(player):
-    print("Agora você pode dar +3 de danos.")
-    player.attack += 3
-
-def get_shield(player):
-    print("Agora você tem +3 de defesa.")
-    player.defense += 3
-
-def get_enchanted_sword(player):
-    print("Agora você pode dar +10 de danos.")
-    player.attack += 10
-
-def get_shield(player):
-    print("Agora você tem +3 de defesa.")
-    player.defense += 3
-
-def get_flaming_shield(player):
-    print("Agora você tem +6 de defesa e +1 de dano")
-    player.defense += 6
-    player.attack += 1
-
-
-
-room = {
-        (1,1) : ["Essa sala contem uma espada. Parabéns !", get_sword],
-        (0,1) : ["Essa sala contem uma espada encantada. Parabéns !", get_enchanted_sword],
-        (2, 2):  ["Você está na primeira sala do castelo.", None],
-        (2,0) : ["Nesta sala tem um T-Rex !!!", fight_trex],
-        (0,0) : ["Nesta sala tem um Dragão Preto !!!", fight_dragon],
-        (1,0) : ["Nesta sala tem um Dragão Laranja !", fight_dragon],
-        (0,2) : ["Você achou um escudo. Ele vai te proteger", get_shield],
-        (1,2) : ["Você achou um escudo em chamas. Ele vai te proteger e machucar os inimigos.", get_flaming_shield]
-        }
+class Enemy:
+    def __init__(self, name, life, defense, attack):
+        self.name = name
+        self.life = life
+        self.defense = defense
+        self.attack = attack
 
 class Fight:
-    def __init__(self, player, enemy):
-        self.player = player
+    def __init__(self, enemy):
+      #  self.player = player
         self.enemy = enemy
 
-    def fight(self):
+    def fight(self, player = None):
+        self.player = player
         while 1:
             print("Você ataca.")
 
@@ -97,12 +59,97 @@ class Fight:
             input()
 
 
-class Enemy:
-    def __init__(self, name, life, defense, attack):
-        self.name = name
-        self.life = life
-        self.defense = defense
-        self.attack = attack
+MONSTERS = {
+"trex": ["T-Rex", 20, 1, 2],
+"monster_frog": ["Sapinho monstro", 30, 2,5],
+"snake":["Cobra dos hits", 40, 1, 8],
+"three_head_dragon": ["Dragão de 3 cabeças", 50, 5, 2],
+"gods_snake": ["Cobra dos deuses", 40, 1, 6],
+"dragon" : ["Dragão", 30, 2, 6],
+"king_dragon": ["Dragão Rei", 100, 8, 20],
+"spikes_monster": ["Monstro de espinhos", 30, 8, 2]
+}
+
+def fight_enemy(name):
+    enemy = Enemy(*MONSTERS[name])
+    fight = Fight(enemy)
+    return fight.fight
+
+
+def get_sword(player):
+    print("Agora você pode dar +3 de danos.")
+    player.attack += 3
+
+def get_enchanted_sword(player):
+    print("Agora você pode dar +10 de danos.")
+    player.attack += 10
+
+def get_shield(player):
+    print("Agora você tem +3 de defesa.")
+    player.defense += 3
+
+def get_flaming_shield(player):
+    print("Agora você tem +6 de defesa e +1 de dano")
+    player.defense += 6
+    player.attack += 1
+
+def get_god_bracelet(player):
+    print("Uma luz aparece no teto, você pega a pulseira e ganha +50 de ataque")
+    player.attack += 50
+
+def get_mega_sword(player):
+    print("O chão se abre e num raio se levanta a mega espada ! Você tem +100 de danos")
+    player.attack += 100
+
+def get_lightning_shield(player):
+    print("Na parede você encontra o Escudo dos Raios. Você tem +100 de defesa e +10 de ataque")
+    player.defense += 100
+    player.attack += 10
+
+def get_life_potion(player):
+    print("Você bebe o frasco e ganha +200 de vida")
+    player.life += 200
+
+def get_mini_life(player):
+    print("Você bebe o frasco e ganha +30 de vida")
+    player.life += 30
+
+def get_protection_googles(player):
+    print("Você coloca os oculos e fica com +20 de proteção")
+    player.defense += 20
+
+def get_gun(player):
+    print("Você ganha +20 de ataque")
+    player.attack += 20
+
+room = {
+        (1,1) : ["Essa sala contem uma espada. Parabéns !", get_sword],
+        (0,1) : ["Essa sala contem uma espada encantada. Parabéns !", get_enchanted_sword],
+        (2, 2):  ["Você está na primeira sala do castelo.", None],
+        (2,0) : ["Nesta sala tem um T-Rex !!!", fight_enemy('trex')],
+        (0,0) : ["Nesta sala tem um Dragão Preto !!!", fight_enemy('dragon')],
+        (1,0) : ["Nesta sala tem um Dragão Laranja !", fight_enemy('dragon')],
+        (0,2) : ["Você achou um escudo. Ele vai te proteger", get_shield],
+        (1,2) : ["Você achou um escudo em chamas. Ele vai te proteger e machucar os inimigos.", get_flaming_shield]
+        }
+
+room = {
+        (0,0) : ["THE KING ESTA NESTA SALA ! TEMA-ME !!!!", fight_enemy('king_dragon')],
+        (1,0) : ["Encontrou a pulseira dos deuses.", get_god_bracelet],
+        (1,1) : ["... Tremores ...", get_mega_sword],
+        (0,1) : ["A sala esta brilhando.", get_lightning_shield],
+        (2,0) : ["PSSSSSSS !!!!!", fight_enemy('gods_snake')],
+        (2,1) : ["Pica pica ...", fight_enemy('spikes_monster')],
+        (3,1) : ["GRRRRR RRRRRRR R R R !", fight_enemy('trex')],
+        (0,2) : ["UEBET UEBET !!!", fight_enemy('monster_frog')],
+        (0,3) : ["Poção de vida !", get_life_potion],
+        (1,3) : ["Vamos ver !", get_protection_googles],
+        (2,3) : ["Uma mini-poção...", get_mini_life],
+        (3,2) : ["Essa sala tem uma arminha !", get_gun],
+        (1,2) : ["Pppissss ...", fight_enemy('snake')],
+        (2,2) : ["3 cabeças ??!!", fight_enemy('three_head_dragon')]
+        }
+
 
 class Player:
     def __init__(self):
@@ -120,10 +167,8 @@ class Player:
             print("Entrou numa sala que já visitou")
         else:
             self.visited.append(position)
-
             if position in room:
                 print(position, " : " , room[position][0], end=' ')
-
                 action = room[position][1]
             else:
                 print("Entrou numa sala sombria .... ")
@@ -136,7 +181,7 @@ class Player:
 
 
     def play(self):
-        self.goto_room((2, 2))
+        self.goto_room((3, 3))
         while 1:
             next_action = input()
             position = self.position
